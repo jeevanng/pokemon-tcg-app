4e7b07ed3fb3a66a168a254bb142f53bca1aee33
jeevanng <jjjeeevvvaaannn@gmail.com>
2023-10-07 10:51:43 +1100

    fix: fix deployment warnings

--- src/context/ApiContext.js
+++ src/context/ApiContext.js
@@ -1,28 +1,27 @@
+import { createContext, useState } from "react";
 
-import { createContext, useState, useEffect } from "react";
 
+export const ApiContext = createContext(null);
 
-export const ApiContext = createContext(null)
+export default function ApiProvider ({children}){
 
-export default function ApiProvider({children}){
+	const [apiUrl, setApiUrl] = useState("https://api.pokemontcg.io/v2/");
 
-    const [apiUrl, setApiUrl] = useState("https://api.pokemontcg.io/v2/");
+	// useEffect(() => {
+	// 	setApiUrl("https://api.pokemontcg.io/");
+	// }, []);
 
-    // useEffect(() => {
-    //     setApiUrl("https://api.pokemontcg.io/");
-    // }, []);
+	return (
+		<ApiContext.Provider value={
+			{
+				api: apiUrl, 
+				setApi: setApiUrl
+			}
+		}>
 
-    return(
-        <ApiContext.Provider value={
-            {
-                api: apiUrl, 
-                setApi: setApiUrl
-            }
-        }>
-            {children}
+			{children}
 
-        </ApiContext.Provider>
-    )
-
+		</ApiContext.Provider>
+	)
 
 }
